da238b17d01b6ec90028cbe3c72e88caa750c06b
Altwath <haidar.altwat@gmail.com>
2021-03-01 20:07:12 +0300

    This is our first commit

---
+++ OneDrive/Desktop/Java/MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+
+   public static void main(String [] args){
+              System.out.print("I love Pizza");
+              System.out.print("It's really good");
+     }
+}
