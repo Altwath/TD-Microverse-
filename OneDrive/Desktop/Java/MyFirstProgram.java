51ae4c51a6fae1b7bad9cb36550a896a5e674e8d
altwath <haidar.altwat@gmail.com>
2021-03-01 20:25:21 +0300

    I changed the code to creat another example where I display the number

--- OneDrive/Desktop/Java/MyFirstProgram.java
+++ OneDrive/Desktop/Java/MyFirstProgram.java
@@ -3,7 +3,7 @@ public class MyFirstProgram {
    public static void main(String [] args){
               
       int x = 123;
-      System.out.println(x); 
+      System.out.println("My number is: "+x); // Assuming I want to display my number in the terminal I will add the qutation mark with a plus sign
               
       
      }
