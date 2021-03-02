5a6751d5fb49d98750689f93dee54b104ea71712
altwath <haidar.altwat@gmail.com>
2021-03-02 20:46:32 +0300

    scanner example where I can input my age wich is only intiger

--- OneDrive/Desktop/Java/MyFirstProgram.java
+++ OneDrive/Desktop/Java/MyFirstProgram.java
@@ -4,10 +4,16 @@ public class MyFirstProgram {
 
    public static void main(String [] args){
               
-   Scanner scanner = new Scanner(System.in);
-   System.out.println("what is your name? ");
-   String name = scanner.nextLine();
-   System.out.println("Hello "+name);
+   Scanner scanner = new Scanner(System.in); //This line here is used for input
+
+   System.out.println("what is your name? "); 
+   String name = scanner.nextLine();       //This line is meant for the programe to accept a name. 
+   System.out.println("Hello "+name);      //This is the message 
+
+   System.out.println("How old are you?"); //We want the program to accept only integer 
+   int age = scanner.nextInt(); //This line is the same as line 10 but it accepts only integrs 
+   System.out.println("You are "+age+" years old");
       
+
      }
 }
