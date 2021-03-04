b6b2aa9cf2bb9e464c212f62015183c82e6979d1
altwath <haidar.altwat@gmail.com>
2021-03-04 09:53:36 +0300

    This is an ecample on how to use expression

--- OneDrive/Desktop/Java/MyFirstProgram.java
+++ OneDrive/Desktop/Java/MyFirstProgram.java
@@ -1,11 +1,25 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class MyFirstProgram {
 
-   public static void main(String [] args){
-              
-      String name = JOptionPane.showInputDialog("Type in your name");
-      JOptionPane.showMessageDialog(null, "Hello "+ name);
+   static double x;
+   static double y;
+   static double z;
+
+   public static void main(String[] args) {
+
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Enter side x: ");
+      x = scanner.nextDouble();
+      System.out.println("Enter side y: ");
+      y = scanner.nextDouble();
+
+      z = Math.sqrt((x * x) + (y * y));
+    System.out.println("The hypotenuse is : "+z);
        
+    scanner.close();
 
      }
 }
