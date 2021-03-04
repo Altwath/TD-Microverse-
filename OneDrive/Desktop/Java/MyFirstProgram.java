7ae1759eb201547adbb4194d2d2076261d78b88c
altwath <haidar.altwat@gmail.com>
2021-03-04 10:12:24 +0300

    Generate Random value

--- OneDrive/Desktop/Java/MyFirstProgram.java
+++ OneDrive/Desktop/Java/MyFirstProgram.java
@@ -1,25 +1,18 @@
 import javax.swing.JOptionPane;
 import java.util.Scanner;
+import java.util.Random;
 
 public class MyFirstProgram {
 
-   static double x;
-   static double y;
-   static double z;
-
+   
    public static void main(String[] args) {
+   
+   Random random = new Random();
+   
+   int X = random.nextInt(6)+1;
 
-      Scanner scanner = new Scanner(System.in);
-
-      System.out.println("Enter side x: ");
-      x = scanner.nextDouble();
-      System.out.println("Enter side y: ");
-      y = scanner.nextDouble();
-
-      z = Math.sqrt((x * x) + (y * y));
-    System.out.println("The hypotenuse is : "+z);
-       
-    scanner.close();
+   System.out.println(X);
+     
 
      }
 }
